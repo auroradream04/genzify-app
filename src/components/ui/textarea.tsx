569936dc8020e086bdd0cb5569749e4ead6f0828
Aurora ❄ <alvs131313@gmail.com>
2024-09-06 16:25:18 +0800

    fix: fix issue with textarea not allowing for shift-entering to create a new line

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-      if (event.key === "Enter" || event.key === "NumpadEnter") {
+      if ((event.key === "Enter" || event.key === "NumpadEnter") && !event.shiftKey) {
         event.preventDefault()
         // Trigger the submit event here
         const form = event.currentTarget.form
@@ -22,7 +22,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] outline-none w-full rounded-md border border-zinc-600 focus-visible:border-zinc-300 transition ease-linear duration-150 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
+          "flex h-[40px] min-h-[40px] outline-none w-full rounded-md border border-zinc-600 focus-visible:border-zinc-300 transition ease-linear duration-150 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
           className
         )}
         ref={ref}
