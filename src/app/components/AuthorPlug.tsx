73ce9579c385b680bdcd1e5cd1738c4a6c427057
Aurora ❄ <alvs131313@gmail.com>
2024-10-27 15:47:01 +0800

    feat: add a link to the original repository

--- src/app/components/AuthorPlug.tsx
+++ src/app/components/AuthorPlug.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 export default function AuthorPlug() {
     return (
-        <div className="w-full text-[9px] mt-2">
-            <span className="text-[rgb(150,150,150)]">
-                MADE WITH ❤️ BY
-            </span>
-            <Link className="pl-1 underline text-blue-500 hover:text-purple-500 transition" href={"https://github.com/auroradream04/"}>
-                AURORA
-            </Link>
-        </div>
-    )
+            <div className="w-full text-[9px] mt-2 flex items-center gap-1">
+                <span className="text-[rgb(150,150,150)]">MADE WITH ❤️ BY</span>
+                <Link
+                    className="underline text-blue-500 hover:text-purple-500 transition"
+                    href={"https://alvinchang.dev"}
+                >
+                    AURORA
+                </Link>
+
+                <span className="text-[rgb(150,150,150)]">CLONE THE PROJECT ON</span>
+                <Link
+                    className="underline text-blue-500 hover:text-purple-500 transition"
+                    href={"https://github.com/auroradream04/genzify-app"}
+                >
+                    GITHUB
+                </Link>
+            </div>
+    );
 }
